e93f622699dc0aa1409847348f9e757b7a319daf
Prabhath Darshana <ndp.mymail@gmail.com>
2018-02-26 23:06:30 +1100

    Components and templating basics

--- components/src/app/app.component.ts
+++ components/src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<p>How do you do?</p>
+  <h1>Hello guys</h1>
+  {{myObj.name}}
+  <ul>
+    <li *ngFor = "let arr of myarr">{{arr}}</li>
+  </ul>
+  <p *ngIf="!myarr">Exists</p>
+  <p *ngIf="myarr[0] == 'am'; then tmpl1 else tmpl2"> </p>
+
+  <ng-template #tmpl1>Temp 1 true</ng-template>
+  <ng-template #tmpl2>No, I don't exists</ng-template>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  myObj = {
+    name : 'Prabhath',
+    age: 28
+  };
+
+  myarr = ['am', 'is', 'are'];
 }
