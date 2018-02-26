bcceadb1c247d824135aaec684e31b251f1f62f3
Prabhath Darshana <ndp.mymail@gmail.com>
2018-02-26 23:19:17 +1100

    Property binding

--- components/src/app/app.component.ts
+++ components/src/app/app.component.ts
@@ -2,25 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<p>How do you do?</p>
-  <h1>Hello guys</h1>
-  {{myObj.name}}
-  <ul>
-    <li *ngFor = "let arr of myarr">{{arr}}</li>
-  </ul>
-  <p *ngIf="!myarr">Exists</p>
-  <p *ngIf="myarr[0] == 'am'; then tmpl1 else tmpl2"> </p>
-
-  <ng-template #tmpl1>Temp 1 true</ng-template>
-  <ng-template #tmpl2>No, I don't exists</ng-template>`,
+  template: `<h1>Hello, world</h1>
+  
+  <button [disabled]="buttonStatus != 'enabled'">My Button</button>
+  <img src="{{angularLogo}}">
+  <img [src]="angularLogo">
+  <img bind-src="angularLogo">  
+    
+    
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  myObj = {
-    name : 'Prabhath',
-    age: 28
-  };
-
-  myarr = ['am', 'is', 'are'];
+  buttonStatus = 'enabled';
+  angularLogo = 'https://angular.io/assets/images/logos/angular/angular.svg';
 }
