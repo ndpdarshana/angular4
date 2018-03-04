43f05f35c3b2bd90c37aff2b0f8bb91f9a69cc21
Prabhath Darshana <ndp.mymail@gmail.com>
2018-03-05 10:24:10 +1100

    Style Class binding

--- stylesheet/src/app/app.component.ts
+++ stylesheet/src/app/app.component.ts
@@ -2,14 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1 [class]="titleClass"> Hello! </h1>
+    <h1 [class.red-title]="cssClass"> Hello! </h1>
+    <h1 [ngClass]="titleClasses"> Hello! </h1>
+  `,
   styles: [`
     h1{
       text-decoration: underline;
-      color: #0000cc;
-    } 
+    }
+
+    .red-title{
+      color:red;
+    }
+    
+    .large-title{
+      font-size: 60px;
+    }
   `]
 })
 export class AppComponent {
   title = 'app';
+  titleClass = 'red-title';
+  cssClass = false;
+
+  titleClasses = {
+    'red-title':false,
+    'large-title':true
+  };
 }
